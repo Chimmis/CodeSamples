18ae00cf08ff3575c3b1e4470a9a8d4815bbbe16
Chimmy <pauliux.adoamitis@gmail.com>
2021-04-08 20:16:16 +0300

    Add samples

---
+++ DependencyInjectionDemo/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a DI container
+            var container = new ServiceCollection();
+
+            // Register the services with the container 
+            container.AddSingleton<App>()
+                .AddSingleton<DoSomethingService>()
+                .AddSingleton<IDoSomethingElseService, DoSomethingElseService>(); // I can also register a service as an interface, this is something a lot of programers over-use. Having an interface here gives me no benefit
+
+            // I get the initialized service from the container and run it
+            var service = container.BuildServiceProvider().GetRequiredService<App>();
+
+            service.Run();
+            Console.ReadKey();
+            // Now what happens here is I NEVER initialize DoSomethingService, the DI container takes care of that for me. 
+            // You can make thousands of services like this, and you will never have to create them by yourself.
+        }
+    }
+
+    // Main service
+    public class App
+    {
+        
+        private readonly DoSomethingService doSomethingService;
+        private readonly IDoSomethingElseService doSomethingElseService;
+
+        // This is called 'Constructor Injection' and it's the most common way of injecting services, you will see and use this A LOT'
+        public App(DoSomethingService doSomethingService, IDoSomethingElseService doSomethingElseService)
+        {
+            this.doSomethingService = doSomethingService;
+            this.doSomethingElseService = doSomethingElseService;
+        }
+
+        public void Run()
+        {
+            doSomethingService.DoSomething();
+            doSomethingElseService.DoSomethingElse();
+
+            // Uncomment this and see what happens
+            // doSomethingElseService.InvokeTheOtherService();
+        }
+    }
+
+    // Just some service
+
+    public class DoSomethingService
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("I am a crazy service and I am doing something");
+        }
+    }
+
+    public interface IDoSomethingElseService
+    {
+        void DoSomethingElse();
+        void InvokeTheOtherService();
+    }
+
+    public class DoSomethingElseService : IDoSomethingElseService
+    {
+        private readonly DoSomethingService doSomethingService;
+
+        // I can inject any service that is registered with dependency injection
+        public DoSomethingElseService(DoSomethingService doSomethingService)
+        {
+            this.doSomethingService = doSomethingService;
+        }
+
+        public void DoSomethingElse()
+        {
+            Console.WriteLine("My programming dick is large and I am quite a vile service");
+        }
+
+        public void InvokeTheOtherService()
+        {
+            // I'm never calling this method in any other service, it's just a demo that you can do this and compose services however the fuck you want'
+            this.doSomethingService.DoSomething();
+        }
+    }
+}
---
+++ InputValidator/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InputValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new ServiceCollection();
+
+            // Two implementations of Ball validator are registered
+            container.AddTransient<IValidator<Ball>, BallSizeValidator>()
+                .AddTransient<IValidator<Ball>, BallPriceValidator>()
+                .AddSingleton<RunnerService>()
+                .AddSingleton<BallCreationService>()
+                .AddSingleton<ValidationService<Ball>>();
+
+            var runner = container.BuildServiceProvider().GetRequiredService<RunnerService>();
+            runner.Run();
+        }
+    }
+
+    public class RunnerService
+    {
+        private readonly BallCreationService ballCreationService;
+        private readonly ValidationService<Ball> ballValidationService;
+
+        public RunnerService(BallCreationService ballCreationService, ValidationService<Ball> ballValidationService)
+        {
+            this.ballCreationService = ballCreationService;
+            this.ballValidationService = ballValidationService;
+        }
+
+        public void Run()
+        {
+            var balls = ballCreationService.CreateBalls();
+
+            // Validate the balls with our created validators
+            var validationOutputs = balls.SelectMany(ball => ballValidationService.Validate(ball));
+
+            if (validationOutputs.Any(x => !x.Success))
+            {
+                foreach (var validationOutput in validationOutputs.Where(x => !x.Success))
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, validationOutput.Errors.Select(x => x.Message)));
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("All balls are valid!");
+            }
+
+            Console.Read();
+        }
+    }
+
+    public class BallCreationService
+    {
+        public List<Ball> CreateBalls()
+        {
+            return new List<Ball>
+            {
+                new Ball(9, -10, "Free ball"),
+                new Ball(1, 1001, "Plain wrong ball"),
+                new Ball(5, 50, "Legit ball"),
+                new Ball(3, 50000, "Pricey ball"),
+            };
+        }
+    }
+
+    public class ValidationService<T> where T: class
+    {
+        private readonly IEnumerable<IValidator<T>> validators;
+
+        public ValidationService(IEnumerable<IValidator<T>> validators)
+        {
+            this.validators = validators;
+        }
+
+        public List<ValidationOutput> Validate(T input)
+        {
+            return validators.Select(validator => validator.Validate(input)).ToList();
+        }
+    }
+
+    public class Ball
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
+
+        public int Price { get; set; }
+
+        public Ball(int size, int price, string name)
+        {
+            this.Size = size;
+            this.Price = price;
+            this.Name = name;
+        }
+    }
+
+    public class BallSizeValidator : IValidator<Ball>
+    {
+        public ValidationOutput Validate(Ball input)
+        {
+            var errors = new List<ValidationError>();
+            if (input.Size < 2)
+            {
+                errors.Add(ValidationError.Create($"{input.Name} : Ball is too small, it needs to be larger than 2."));
+            }
+
+            if (input.Size > 10)
+            {
+                errors.Add(ValidationError.Create($"{input.Name} : Ball is too large, it needs to be smaller than 10."));
+            }
+
+            return ValidationOutput.Create(errors);
+        }
+    }
+
+    public class BallPriceValidator : IValidator<Ball>
+    {
+        public ValidationOutput Validate(Ball input)
+        {
+            var errors = new List<ValidationError>();
+            if (input.Price <= 0)
+            {
+                errors.Add(ValidationError.Create($"{input.Name} : Ball price needs to be larger than 0, we'll go bankrupt if we give shit away for free."));
+            }
+
+            if (input.Price > 1000)
+            {
+                errors.Add(ValidationError.Create($"{input.Name} : No ball is that expensive ({input.Price}), it needs to be less than 1000."));
+            }
+
+            return ValidationOutput.Create(errors);
+        }
+    }
+
+
+    public interface IValidator<T> 
+        where T: class
+    {
+        ValidationOutput Validate(T input);
+    }
+
+    public class ValidationOutput
+    {
+        public List<ValidationError> Errors { get; }
+
+        public bool Success => Errors.Count == 0;
+
+        private ValidationOutput(List<ValidationError> errors)
+        {
+            this.Errors = errors;
+        }
+
+        public static ValidationOutput Create (List<ValidationError> errors)
+        {
+            return new ValidationOutput(errors);
+        }
+    }
+
+    public class ValidationError
+    {
+        // This structure ensures, that once the message is created - it can not be changed
+        public string Message { get; }
+
+        private ValidationError(string message)
+        {
+            this.Message = message;
+        }
+
+        public static ValidationError Create(string message)
+        {
+            return new ValidationError(message);
+        }
+    }
+}
